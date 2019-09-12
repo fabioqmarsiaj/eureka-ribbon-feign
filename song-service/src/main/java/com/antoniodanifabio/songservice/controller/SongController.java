3cb061d6db97e60e6ca8d61142772381394ae611
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2019-09-12 15:37:15 -0300

    Adicionado dani nos packages e nos build.gradle group

--- song-service/src/main/java/com/antoniodanifabio/songservice/controller/SongController.java
+++ song-service/src/main/java/com/antoniodanifabio/songservice/controller/SongController.java
@@ -1,12 +1,10 @@
 package com.antoniofabio.songservice.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.antoniofabio.songservice.domain.Song;
 import com.antoniofabio.songservice.repository.SongRepository;
 
