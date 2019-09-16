e2613f9114036ab136a3f8aa5b4fa2d8f8ac9924
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2019-09-16 15:36:01 -0300

    PLAYLIST: registrando no eureka pelo feign

--- playlist-service/src/main/java/com/antoniodanifabio/playlistservice/PlaylistServiceApplication.java
+++ playlist-service/src/main/java/com/antoniodanifabio/playlistservice/PlaylistServiceApplication.java
@@ -2,13 +2,14 @@ package com.antoniodanifabio.playlistservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PlaylistServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlaylistServiceApplication.class, args);
-
     }
 
 }
--- playlist-service/src/main/java/com/antoniodanifabio/playlistservice/discovery/SpringBootApplicationDown.java
+++ playlist-service/src/main/java/com/antoniodanifabio/playlistservice/discovery/SpringBootApplicationDown.java
@@ -1,18 +1,18 @@
 package com.antoniodanifabio.playlistservice.discovery;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SpringBootApplicationStartup implements ApplicationListener<ApplicationReadyEvent> {
+public class SpringBootApplicationDown implements ApplicationListener<ContextClosedEvent> {
 
     @Autowired
     private EurekaOperations eurekaOperations;
 
     @Override
-    public void onApplicationEvent(final ApplicationReadyEvent event) {
-        eurekaOperations.register();
+    public void onApplicationEvent(ContextClosedEvent event) {
+        eurekaOperations.delete();
     }
 }
