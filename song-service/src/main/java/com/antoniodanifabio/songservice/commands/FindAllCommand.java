01e186c5f36cf1cc22c0cb10a4b3c12f513254f8
daniinyan <daniela.dbass@gmail.com>
2019-09-17 14:13:47 -0300

    SONG: Adiciona espaço em assinatura de classe

--- song-service/src/main/java/com/antoniodanifabio/songservice/commands/FindAllCommand.java
+++ song-service/src/main/java/com/antoniodanifabio/songservice/commands/FindAllCommand.java
@@ -7,7 +7,7 @@ import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandProperties;
 
-public class FindAllCommand extends HystrixCommand<List<Song>>{
+public class FindAllCommand extends HystrixCommand<List<Song>> {
 	
 	private SongRepository repository;
 	
