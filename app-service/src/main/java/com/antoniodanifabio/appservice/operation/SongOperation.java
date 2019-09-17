6c295ea1dbcbc8d2a56d4162bbe6bff68a5ead76
Antonio Azambuja <antonioazambuja_@hotmail.com>
2019-09-17 17:09:31 -0300

    APP: Remove métodos e classes não utilizados

--- app-service/src/main/java/com/antoniodanifabio/appservice/operation/SongOperation.java
+++ app-service/src/main/java/com/antoniodanifabio/appservice/operation/SongOperation.java
@@ -1,22 +1,10 @@
 package com.antoniodanifabio.appservice.operation;
 
 import com.antoniodanifabio.appservice.domain.Song;
-import feign.Body;
-import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
 
-import java.util.List;
-
 public interface SongOperation {
-
-    @RequestLine("GET /songs")
-    List<Song> getAllSongs();
-
-    @RequestLine("POST /songs")
-    @Headers("Content-Type: application/json")
-    @Body("{jsonSong}")
-    Song insertSong(@Param("jsonSong") String jsonSong);
 
     @RequestLine("GET /songs/{songId}")
     Song searchById(@Param("songId") String songId);
