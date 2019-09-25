68c81c17aa812bfbfbe179e815091a85c7640931
Fabio Marsiaj <fabiomarsiaj@gmail.com>
2019-09-25 00:28:21 -0300

    PLAYLIST: Modelando a classe json

--- playlist-service/src/main/java/com/antoniodanifabio/playlistservice/discovery/EurekaOperations.java
+++ playlist-service/src/main/java/com/antoniodanifabio/playlistservice/discovery/EurekaOperations.java
@@ -2,8 +2,10 @@ package com.antoniodanifabio.playlistservice.discovery;
 
 import feign.Feign;
 import feign.gson.GsonDecoder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import com.antoniodanifabio.playlistservice.domain.RegistryJson;
 
 @Component
 public class EurekaOperations {
@@ -16,6 +18,8 @@ public class EurekaOperations {
     private String hostName;
     @Value("${service.name}")
     private String serviceName;
+    @Autowired
+    private RegistryJson registryJson;
 
     private  EurekaHttpClient eurekaHttpMethodsService = Feign
             .builder()
@@ -23,6 +27,20 @@ public class EurekaOperations {
             .target(EurekaHttpClient.class, "http://localhost:8080/eureka/v2/apps");
 
     public void register(){
+    	
+    	registryJson.setHostName(buildInstanceID(ipAddress, serverPort, serviceName));
+    	registryJson.setApp(serviceName);
+    	registryJson.setIpAddr(ipAddress);
+    	registryJson.getPort().add(serverPort);
+    	registryJson.getPort().add("true");
+    	registryJson.getSecurePort().add("8443");
+    	registryJson.getSecurePort().add("true");
+    	registryJson.setHealthCheckUrl("http://localhost:"+serverPort+"healthcheck");
+    	registryJson.setStatusPageUrl("http://localhost:"+serverPort+"status");
+    	registryJson.setHomePageUrl("http://localhost:"+serverPort);
+    	registryJson.getDataCenterInfo().add("com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo");
+    	registryJson.getDataCenterInfo().add("MyOwn");
+    	
         eurekaHttpMethodsService.registry(
                 "{\n" +
                         "    \"instance\": {\n" +
--- playlist-service/src/main/java/com/antoniodanifabio/playlistservice/domain/RegistryJson.java
+++ playlist-service/src/main/java/com/antoniodanifabio/playlistservice/domain/RegistryJson.java
@@ -1,11 +1,10 @@
 package com.antoniodanifabio.playlistservice.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.stereotype.Component;
 
-import javax.sound.sampled.Port;
-
-import com.netflix.appinfo.InstanceInfo;
-
+@Component
 public class RegistryJson {
 	
 	private String hostName;
@@ -14,12 +13,80 @@ public class RegistryJson {
 	private final String secureVipAddress = "com.localhost";
 	private String ipAddr;
 	private String status = "STARTING";
-	private Port port;
-	private final String securePort = "8443";
+	private List<String> port;
+	private List<String> securePort;
 	private String healthCheckUrl;
 	private String statusPageUrl;
 	private String homePageUrl;
-	private InstanceInfo dataCenterInfo;
+	private List<String> dataCenterInfo;
+	
+	public RegistryJson() {
+		super();
+		port = new ArrayList<String>();
+		securePort = new ArrayList<String>();
+		dataCenterInfo = new ArrayList<String>();
+	}
+	
+	public String getHostName() {
+		return hostName;
+	}
+	public void setHostName(String hostName) {
+		this.hostName = hostName;
+	}
+	public String getApp() {
+		return app;
+	}
+	public void setApp(String app) {
+		this.app = app;
+	}
+	public String getIpAddr() {
+		return ipAddr;
+	}
+	public void setIpAddr(String ipAddr) {
+		this.ipAddr = ipAddr;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public List<String> getPort() {
+		return port;
+	}
+	public List<String> getSecurePort() {
+		return securePort;
+	}
+	public String getHealthCheckUrl() {
+		return healthCheckUrl;
+	}
+	public void setHealthCheckUrl(String healthCheckUrl) {
+		this.healthCheckUrl = healthCheckUrl;
+	}
+	public String getStatusPageUrl() {
+		return statusPageUrl;
+	}
+	public void setStatusPageUrl(String statusPageUrl) {
+		this.statusPageUrl = statusPageUrl;
+	}
+	public String getHomePageUrl() {
+		return homePageUrl;
+	}
+	public void setHomePageUrl(String homePageUrl) {
+		this.homePageUrl = homePageUrl;
+	}
+	public List<String> getDataCenterInfo() {
+		return dataCenterInfo;
+	}
+	public String getVipAddress() {
+		return vipAddress;
+	}
+	public String getSecureVipAddress() {
+		return secureVipAddress;
+	}
+	
+	
+	
 	
 	
 	
